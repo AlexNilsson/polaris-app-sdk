b5d8f24944173ab548a5df35e51e53c5f79147e8
Alexander Nilsson <alexandernilsson12@gmail.com>
2021-12-10 00:11:32 +0100

    feat: initial release

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,14 +1,12 @@
 import * as polaris from './index'
 
-function init () {
-  polaris.initApp({
-    getProperties: () => ({ a: 1, b: 2 }),
-    setProperties: (x) => {},
-  })
-}
-
 describe('Polaris App SDK', () => {
   test('should init correctly', () => {
-    expect(init()).not.toThrow()
+    expect(() => {
+      polaris.initApp({
+        getProperties: () => ({ a: 1, b: 2 }),
+        setProperties: (x) => {},
+      })
+    }).not.toThrow()
   })
 })
